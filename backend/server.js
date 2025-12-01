de6b753acd1f2b8cfbe01166644086af53bdca66
Farhad Nuri <231006312@eastdelta.edu.bd>
2025-12-01 20:40:29 +0600

    Fix backend for API-only deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express"
 import dotenv from "dotenv"
-import path from "path"
 import cors from "cors"
 import { connectDB } from "./lib/db.js"
 import problemRoute from "./routes/problem.route.js"
@@ -9,24 +8,15 @@ dotenv.config()
 
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-
-const __dirname = path.resolve()
 
 // API routes
 app.use("/api/problem", problemRoute)
 app.use("/api/pattern", patternRoute)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")))
-
-    app.get(/^(?!\/api).*/, (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-}
 app.listen(PORT, () => {
     console.log("Server running on port ", PORT)
     connectDB()
